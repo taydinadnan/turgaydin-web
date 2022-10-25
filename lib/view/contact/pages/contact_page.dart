dc61d8450924d707872e23c0e3b0908989d30df2
Turgay Aydin <taydinadnan@gmail.com>
2022-10-25 14:40:32 +0200

    contact me page and functionality

--- lib/view/contact/pages/contact_page.dart
+++ lib/view/contact/pages/contact_page.dart
@@ -1,13 +1,80 @@
+import 'package:contactus/contactus.dart';
 import 'package:flutter/material.dart';
+import 'package:mb_contact_form/mb_contact_form.dart';
+import 'package:turgaydin/utils/responsive.dart';
 
 class ContactPage extends StatelessWidget {
-  const ContactPage({Key? key}) : super(key: key);
+  const ContactPage({Key? key, required this.screenSize}) : super(key: key);
+
+  final Size screenSize;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: const Center(child: Text("HOME PAGE")),
-    );
+    return ResponsiveWidget.isSmallScreen(context)
+        ? Column(
+            children: [
+              ContactUs(
+                cardColor: Colors.white,
+                textColor: Colors.black,
+                email: 'taydinadnan@gmail.com',
+                companyName: 'Adnan Turgay Aydin',
+                companyColor: Colors.red.shade400,
+                dividerThickness: 2,
+                phoneNumber: '+387644057832',
+                website: 'https://turgayaydin.com',
+                githubUserName: 'taydinadnan',
+                linkedinURL: 'https://www.linkedin.com/in/taydinadnan/',
+                tagLine: 'Flutter Developer',
+                taglineColor: Colors.red.shade400,
+                twitterHandle: 'turgayaydin',
+                instagram: 'turgaydin',
+                dividerColor: Colors.red.shade400,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 110.0),
+                child: MBContactForm(
+                  hasHeading: true,
+                  withIcons: false,
+                  destinationEmail: "taydinadnan@gmail.com",
+                ),
+              ),
+            ],
+          )
+        : SizedBox(
+            width: screenSize.width,
+            child: Row(
+              children: [
+                Expanded(
+                  child: ContactUs(
+                    cardColor: Colors.white,
+                    textColor: Colors.black,
+                    email: 'taydinadnan@gmail.com',
+                    companyName: 'Adnan Turgay Aydin',
+                    companyColor: Colors.red.shade400,
+                    dividerThickness: 2,
+                    phoneNumber: '+387644057832',
+                    website: 'https://turgayaydin.com',
+                    githubUserName: 'taydinadnan',
+                    linkedinURL: 'https://www.linkedin.com/in/taydinadnan/',
+                    tagLine: 'Flutter Developer',
+                    taglineColor: Colors.red.shade400,
+                    twitterHandle: 'turgayaydin',
+                    instagram: 'turgaydin',
+                    dividerColor: Colors.red.shade400,
+                  ),
+                ),
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 110.0),
+                    child: MBContactForm(
+                      hasHeading: true,
+                      withIcons: false,
+                      destinationEmail: "taydinadnan@gmail.com",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
