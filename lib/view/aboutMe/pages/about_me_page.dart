6614e69d339925ce0227d4211f9a6db85b45d839
Turgay Aydin <taydinadnan@gmail.com>
2022-10-25 13:38:26 +0200

    about me tabs

--- lib/view/aboutMe/pages/about_me_page.dart
+++ lib/view/aboutMe/pages/about_me_page.dart
@@ -1,13 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:tab_container/tab_container.dart';
+import 'package:turgaydin/utils/responsive.dart';
+import 'package:turgaydin/utils/styles.dart';
+import 'package:turgaydin/view/aboutMe/widgets/education.dart';
+import 'package:turgaydin/view/aboutMe/widgets/personal.dart';
+import 'package:turgaydin/view/aboutMe/widgets/skills.dart';
 
 class AboutMePage extends StatelessWidget {
-  const AboutMePage({Key? key}) : super(key: key);
+  const AboutMePage({Key? key, required this.screenSize}) : super(key: key);
+
+  final Size screenSize;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: const Center(child: Text("HOME PAGE")),
-    );
+    return ResponsiveWidget.isSmallScreen(context)
+        ? SizedBox(
+            width: screenSize.width / 1,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 50.0,
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: CircleAvatar(
+                          foregroundImage: const AssetImage(
+                            "assets/logo/pp2.jpg",
+                          ),
+                          radius: screenSize.height / 7,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: SizedBox(
+                        width: screenSize.width / 1,
+                        height: screenSize.height / 2.5,
+                        child: TabContainer(
+                          colors: <Color>[
+                            Colors.red.shade200,
+                            const Color(0xffa275e3),
+                            const Color(0xff9aebed),
+                          ],
+                          tabs: const [
+                            'Personal',
+                            'Education',
+                            'Skills',
+                          ],
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsets.only(left: screenSize.width / 10),
+                              child: const PersonalTab(),
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsets.only(left: screenSize.width / 10),
+                              child: const EducationTab(),
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsets.only(left: screenSize.width / 10),
+                              child: const SkillsTab(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        : Padding(
+            padding: EdgeInsets.only(top: 50.0, left: screenSize.width / 4.5),
+            child: Row(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: CircleAvatar(
+                    foregroundImage: const AssetImage(
+                      "assets/logo/pp2.jpg",
+                    ),
+                    radius: screenSize.height / 7,
+                  ),
+                ),
+                const SizedBox(width: 25),
+                SizedBox(
+                  width: screenSize.width / 3.5,
+                  height: screenSize.height / 2.5,
+                  child: TabContainer(
+                    colors: <Color>[
+                      Colors.red.shade200,
+                      const Color(0xffa275e3),
+                      const Color(0xff9aebed),
+                    ],
+                    tabs: const [
+                      'Personal',
+                      'Education',
+                      'Skills',
+                    ],
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: screenSize.width / 30),
+                        child: const PersonalTab(),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: screenSize.width / 30),
+                        child: const EducationTab(),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: screenSize.width / 30),
+                        child: const SkillsTab(),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
---
+++ lib/view/aboutMe/widgets/personal.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:turgaydin/utils/styles.dart';
+
+class PersonalTab extends StatelessWidget {
+  const PersonalTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Adnan Turgay Aydin",
+              style: TaydinStyles.poppins16Bold.copyWith(fontSize: 20),
+              overflow: TextOverflow.ellipsis,
+            ),
+            Wrap(
+              children: const [
+                Text(
+                  "Nationality :",
+                  style: TaydinStyles.poppins16Bold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  " Turkey",
+                  style: TaydinStyles.poppins16SemiBold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+            Wrap(
+              children: const [
+                Text(
+                  "Age: ",
+                  style: TaydinStyles.poppins16Bold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  "26.04.1997",
+                  style: TaydinStyles.poppins16SemiBold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+            Wrap(
+              children: const [
+                Text(
+                  "Profession : ",
+                  style: TaydinStyles.poppins16Bold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TaydinStyles.poppins16SemiBold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+            Wrap(
+              children: const [
+                Text(
+                  "Location: ",
+                  style: TaydinStyles.poppins16Bold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  "Sarajevo, BiH",
+                  style: TaydinStyles.poppins16SemiBold,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ],
+        ),
+        Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                "Languages:",
+                style: TaydinStyles.poppins16Bold,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                "Turkish",
+                style: TaydinStyles.poppins16SemiBold,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                "English",
+                style: TaydinStyles.poppins16SemiBold,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                "Bosnian",
+                style: TaydinStyles.poppins16SemiBold,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
